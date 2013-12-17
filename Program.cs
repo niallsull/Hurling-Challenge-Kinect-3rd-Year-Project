5eac20eb44f398ba53523f88ca388ac09e8759f5
niallsull <niallsull@yahoo.co.uk>
2013-12-17 11:36:42 +0000

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,23 @@
+// typical program cs of xna
+
+using System;
+
+namespace KinectWithGameState
+{
+#if WINDOWS || XBOX
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            using (Game1 game = new Game1())
+            {
+                game.Run();
+            }
+        }
+    }
+#endif
+}
+
